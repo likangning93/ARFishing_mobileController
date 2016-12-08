087f00406721f645a2ef1606c006a91ba80ebb8a
Kangning Li <likangning93@gmail.com>
2016-12-08 00:38:56 -0500

    acceleration handling appears on phone. onward to networking!

--- ARFishing_MobileController/Assets/Scripts/PhoneNetwork.cs
+++ ARFishing_MobileController/Assets/Scripts/PhoneNetwork.cs
@@ -4,10 +4,18 @@ using System.Collections;
 public class PhoneNetwork : Photon.PunBehaviour
 {
 
+    float accelMax = 0.0f;
+
     void OnGUI()
     {
         GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
         GUILayout.Label("Room Name: " + roomName);
+        GUILayout.Label("Accelerometer: " + Input.acceleration.ToString());
+
+        accelMax = Mathf.Max(accelMax, Input.acceleration.magnitude);
+        GUILayout.Label("Max Accel: " + accelMax);
+
+        GUILayout.Label("Gyro: " + Input.gyro.attitude.ToString());
     }
 
     string roomName;
@@ -15,6 +23,14 @@ public class PhoneNetwork : Photon.PunBehaviour
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            accelMax = 0.0f;
+        }
     }
 
     // TODO-2.a: the same as 1.b
