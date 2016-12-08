97bb70be98b5c0aedfc558e5b7407033678d7c72
Kangning Li <likangning93@gmail.com>
2016-12-07 20:27:57 -0500

    initial commit with photon

---
+++ ARFishing_MobileController/Assets/Scripts/PhoneController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhoneController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Activate()
+    {
+
+    }
+}
---
+++ ARFishing_MobileController/Assets/Scripts/PhoneNetwork.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhoneNetwork : Photon.PunBehaviour
+{
+
+    void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+        GUILayout.Label("Room Name: " + roomName);
+    }
+
+    string roomName;
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+    }
+
+    // TODO-2.a: the same as 1.b
+    //   and join a room
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        roomName = PhotonNetwork.room.name;
+        GetComponent<PhoneController>().Activate();
+    }
+}
